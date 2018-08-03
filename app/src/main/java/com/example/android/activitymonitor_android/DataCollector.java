fb3c0fa497d585c3c313feaeb1f6543340868e60
jtwwang <justin.w.wang.0@gmail.com>
2018-08-02 19:50:40 -0700

    STRINGS ARE IMMUTABLE. GOING BACK TO MASTER TO FIX.

--- app/src/main/java/com/example/android/activitymonitor_android/DataCollector.java
+++ app/src/main/java/com/example/android/activitymonitor_android/DataCollector.java
@@ -6,6 +6,12 @@ import android.content.Intent;
 import android.content.Context;
 import android.os.IBinder;
 import android.util.Log;
+import android.app.usage.UsageStatsManager;
+import android.app.usage.UsageStats;
+
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -34,7 +40,7 @@ public class DataCollector extends IntentService {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
-       // onHandleIntent(intent);
+        //onHandleIntent(intent);
         return START_STICKY; //Will re-create after process is killed
 
     }
@@ -49,47 +55,41 @@ public class DataCollector extends IntentService {
 
     //TODO: implement observer
         //Possible use get context or refresh to receive app data
+
+
+    private String printForegroundTask() {
+        String currentApp = "NULL";
+        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            UsageStatsManager usm = (UsageStatsManager)this.getSystemService("usagestats");
+            long time = System.currentTimeMillis();
+            List<UsageStats> appList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,  time - 1000*1000, time);
+            if (appList != null && appList.size() > 0) {
+                SortedMap<Long, UsageStats> mySortedMap = new TreeMap<Long, UsageStats>();
+                for (UsageStats usageStats : appList) {
+                    mySortedMap.put(usageStats.getLastTimeUsed(), usageStats);
+                }
+                if (mySortedMap != null && !mySortedMap.isEmpty()) {
+                    currentApp = mySortedMap.get(mySortedMap.lastKey()).getPackageName();
+                }
+            }
+        } else {
+            ActivityManager am = (ActivityManager)this.getSystemService(Context.ACTIVITY_SERVICE);
+            List<ActivityManager.RunningAppProcessInfo> tasks = am.getRunningAppProcesses();
+            currentApp = tasks.get(0).processName;
+        }
+
+        Log.e("adapter", "Current App in foreground is: " + currentApp);
+        return currentApp;
+    }
+
     protected void onHandleIntent(Intent workIntent) {
         Log.e("test", "reached");
-        String foregroundTaskPackageName;
-        String newTaskPackageName;
-
-        ActivityManager actMan = (ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE);
-        if (android.os.Build.VERSION.SDK_INT < 20) {
-            ActivityManager.RunningTaskInfo foreTask = actMan.getRunningTasks(1).get(0);
-            foregroundTaskPackageName = foreTask.topActivity.getPackageName();
-        }
-        else {
-            ActivityManager.RunningAppProcessInfo foreTask = actMan.getRunningAppProcesses().get(0);
-            foregroundTaskPackageName = foreTask.processName;
-        }
-        Log.i("DataCollector",  foregroundTaskPackageName);
-        try{
-            Thread.sleep(10000);
-        }catch(InterruptedException ex){
-            //do stuff
-        }
-        // newActMan goes out of scope anyway. 
-        do{
-
-            ActivityManager newActMan = (ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE);
-            // introduce some type of time delay here
-            if (android.os.Build.VERSION.SDK_INT < 20) {
-                ActivityManager.RunningTaskInfo foreTask = newActMan.getRunningTasks(1).get(0);
-                newTaskPackageName = foreTask.topActivity.getPackageName();
-            }
-            else {
-                ActivityManager.RunningAppProcessInfo foreTask = newActMan.getRunningAppProcesses().get(0);
-                newTaskPackageName = foreTask.processName;
-            }
-            Log.i("DataCollector",  newTaskPackageName);
-            // TODO: sleep for 10 secs. Do not use thread.sleep. Try: ScheduledExecutorService and either scheduleAtFixedRate or scheduleWithFixedDelay.
-            // Will change later, just for debugging purposes -> May also be ok to use thread.sleep since we are running in a different thread now?
-            try{
-                Thread.sleep(1000);
-            }catch(InterruptedException ex){
-                //do stuff
-            }
+        String newTaskPackageName = "NULL";
+        String foregroundTaskPackageName = "NULL";
+        foregroundTaskPackageName = printForegroundTask();
+        do {
+            foregroundTaskPackageName = newTaskPackageName;
+            newTaskPackageName = printForegroundTask();
         }while(newTaskPackageName.equals(foregroundTaskPackageName));
 
         // This is where we "return" since the foreground app has now changed
